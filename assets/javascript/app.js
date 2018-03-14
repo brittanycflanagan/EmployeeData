5cace1c071e5fb9670fed5794d892ff55ce45994
unknown <brittanycturner@gmail.com>
2018-03-13 20:29:21 -0700

    just updating html & java

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,28 +1,6 @@
 
-$("#submit").on("click", function() {
-    event.preventDefault();
-    var name = $("#name").val().trim();
-    var role = $("#role").val().trim();
-    var date = $("#date").val().trim();
-    var rate = $("#rate").val().trim();
-    console.log(name);
-    console.log(role);
-    console.log(date);
-    console.log(rate);
-    var tr = $("<tr>")
-    var tdName = $("<td>").text(name);
-    var tdRole = $("<td>").text(role);
-    var tdDate = $("<td>").text(date);
-    var tdWorked = $("<td>");
-    var tdRate = $("<td>").text(rate);
-    var tdBilled = $("<td>");
-    tr.append(tdName).append(tdRole).append(tdDate).append(tdWorked).append(tdRate).append(tdBilled);
-    $("#newRow").append(tr);
-        
-});
-
-
 // Initialize Firebase
+
 var config = {
     apiKey: "AIzaSyCcPVCxkJ8y2UTq7WEAlM36UXBb5uFZtac",
     authDomain: "employee-data-30fb2.firebaseapp.com",
@@ -31,4 +9,50 @@ var config = {
     storageBucket: "employee-data-30fb2.appspot.com",
     messagingSenderId: "841144224128"
   };
-  firebase.initializeApp(config);
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
+ 
+
+
+
+//Submit Button
+$("#submit").on("click", function() {
+    event.preventDefault();
+    var name = $("#name").val().trim();
+    var role = $("#role").val().trim();
+    var date = parseInt($("#date").val().trim());
+    var rate = parseInt($("#rate").val().trim());
+   
+    database.ref().push({
+        name: name,
+        role: role,
+        date: date,
+        rate: rate,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+      });
+
+    console.log(name);
+    console.log(role);
+    console.log(date);
+    console.log(rate);
+    var tr = $("<tr>")
+    var tdName = $("<td>").text(name);
+    var tdRole = $("<td>").text(role);
+    var tdDate = $("<td>").text(date);
+    var tdWorked = $("<td>"); // (calculate months)
+    var tdRate = $("<td>").text(rate);
+    var tdBilled = $("<td>"); // (rate*months)
+    tr.append(tdName).append(tdRole).append(tdDate).append(tdWorked).append(tdRate).append(tdBilled);
+    $("#newRow").append(tr);
+
+    
+        
+});
+
+
+
+
+
+
